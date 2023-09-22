1608e36e1cbddb6b98a34dc8ecfa0feaeafcb4d2
François Guérin <francoisguerin1@hotmail.fr>
2023-09-22 20:15:03 +0200

    Store customs in their own file
    Custom variables and faces pollute `init.el`. Storing them in their
    own file, `custom.el`, not tracked by Git.

--- emacs/init.el
+++ emacs/init.el
@@ -1,14 +1,4 @@
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(orderless auto-package-update vue-mode yaml-mode dotenv-mode markdown-mode typescript-mode json-mode js2-mode eslint-rc corfu tree-sitter-langs eglot consult all-the-icons-completion marginalia vertico magit all-the-icons which-key exec-path-from-shell helpful use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
