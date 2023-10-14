9854970659bf66e01ed266312c110bb90ff52f5c
François Guérin <francoisguerin1@hotmail.fr>
2023-10-14 16:52:05 +0200

    Modify configuration for Emacs29

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file))
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
